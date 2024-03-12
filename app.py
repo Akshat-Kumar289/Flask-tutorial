803a8019d406193ca686f02b956f6826e8a24549
akshat kumar <akshat.kumar@datagrokr.com>
2024-03-12 16:08:56 +0530

    added dynamic route

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id":1,
+        "title": "data engineering",
+        "location": "bengaluru, India"
+    },
+    {
+        "id":2,
+        "title": "data science",
+        "location": "bengaluru, India"
+    },
+    {
+        "id":3,
+        "title": "data excel",
+        "location": "bengaluru, India"
+    },
+    {
+        "id":4,
+        "title": "data play",
+        "location": "bengaluru, India"
+    }
+]
+
+
 @app.route("/")
 def hello_word():
-    return "hello akshat"
+    return render_template('home.html', jobs = JOBS, cn = "akshat")
+
+@app.route("/api/cool")
+def x():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
